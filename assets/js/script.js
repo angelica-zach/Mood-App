d3cf959c6930df4d3d09898a0d7b29d3a37d6b08
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-12-20 20:17:03 +0000

    Initial Pseudocode outline

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// MoodHub Application
+
+// Mood Selection
+    // display mood cards and message to the user
+    //get user input, mood card selection
+
+// Artist & Playlist Info
+    //Youtube API & fetch
+    // Display playlist to user
+    // Musixmatch API & fetch
+    // Display artist and playlist information to the user
+
+
+// BONUS - Venue Info
+    // BONUS - Google Maps API
