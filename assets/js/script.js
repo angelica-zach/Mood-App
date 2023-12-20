662eb3cc6a8a4b2a9654ea6593045b953cde2c41
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-12-20 21:22:52 +0000

    JS API template code, moods array & displayMoodCards function starter code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,33 @@
 // MoodHub Application
 
 // API Keys
+const youtubeApiKey = '';
+const musixmatchApiKey = '';
+const googleMapsApiKey = '';
 
+// API URLs
+const youtubeApiUrl = '';
+const musixmatchApiUrl = '';
+const googleMapsApiUrl = '';
 
 // Mood Selection
-    // Array -  to store moon cards
-    // Function - display mood cards and message to the user
+    // Array -  to store moods
+    const moods = ['Happy', 'Chill', 'Energetic', 'Relaxed', 'Excited', 'Calm', 'Upbeat', 'Peaceful', 'Playful'];
+    // Function - mood cards and message
+    function displayMoodCards() {
+        const moodCardsContainer = document.getElementById('moodCards');
+    }
     // Form - get user input, mood card selection
     // Function - to clear form
 
 // Artist & Playlist Info
-    //Youtube API & fetch
+    // Fetch - Youtube data
     // Display playlist to user
-    // Musixmatch API & fetch
+    // Fetch - Musixmatch data
     // Display artist and playlist information to the user
-
-
+    
 // BONUS - Venue Info
-    // BONUS - Google Maps API
+    // BONUS - Google Maps API data
+
+// Initial display of mood cards on page load
+displayMoodCards();
