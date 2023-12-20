7d872daec925b003fc8f3a2ff633cb2b0a93be8e
angelica-zach <147160016+angelica-zach@users.noreply.github.com>
2023-12-20 20:31:19 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,16 @@
 
 // Mood Selection
     // display mood cards and message to the user
+//have several premade cards? with a drop down sel
     //get user input, mood card selection
+//local storage ? collect stats?
 
 // Artist & Playlist Info
     //Youtube API & fetch
     // Display playlist to user
     // Musixmatch API & fetch
     // Display artist and playlist information to the user
+//get image
 
 
 // BONUS - Venue Info
