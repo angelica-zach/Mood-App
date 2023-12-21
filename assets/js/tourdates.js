36cc2b88e306610bbb0fa3c927e52777d78d63e7
Tom Fahy <thomas.fahy9@gmail.com>
2023-12-21 21:07:18 +0000

    add function to get event data

--- assets/js/tourdates.js
+++ assets/js/tourdates.js
@@ -1,7 +1,7 @@
 // function to provide data on whether an artist is playing near-by.
 // takes a single argument 'artist' which is the name of the artist.
 
-function searchBandsInTown(artist) {
+function searchArtistData(artist) {
     // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=1234";
     fetch(queryURL)
@@ -15,4 +15,18 @@ function searchBandsInTown(artist) {
       });
   }
 
-searchBandsInTown("Taylor Swift"); //an example to show taylor swift tour dates.
+function searchEvents(artist) {
+    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events/?app_id=1234";
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Printing the entire object to console
+        console.log(data);
+      });
+  }
+
+searchArtistData("Taylor Swift"); //an example to show taylor swift tour dates.
+searchEvents("Taylor Swift");
