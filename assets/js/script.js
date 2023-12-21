a10e47c05da89fa1e6728cb4b370a7432d80c72a
Clive Makongo <clivemakongo@gmail.com>
2023-12-21 19:23:36 +0000

    functioning spotify API calls

---
+++ assets/js/script.js
@@ -0,0 +1,121 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const storedSearchesContainer = document.getElementById(
+    "storedSearchesContainer"
+  );
+  const searchInput = document.getElementById("searchInput");
+  const moodPlaylistsContainer = document.getElementById(
+    "moodPlaylistsContainer"
+  );
+  const spotifyPlayerContainer = document.getElementById(
+    "spotifyPlayerContainer"
+  );
+
+  let searchInputValue = "";
+  let accessToken = "";
+  let moodPlaylists = [];
+  let selectedPlaylistUri = "";
+
+  // Define your functions here
+
+  function getSearchInput() {
+    searchInputValue = searchInput.value;
+    console.log("Search input value:", searchInputValue);
+    search(searchInputValue);
+  }
+
+  function search(searchValue) {
+    authenticateSpotify()
+      .then((accessToken) => {
+        const searchEndpoint = `https://api.spotify.com/v1/search?q=${searchValue}&type=playlist`;
+
+        return axios.get(searchEndpoint, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+      })
+      .then((response) => {
+        const playlists = response.data.playlists.items;
+        console.log("Playlists:", playlists);
+
+        // Render the retrieved playlists
+        renderMoodPlaylists(playlists);
+      })
+      .catch((error) => {
+        console.error("Error searching for playlists:", error);
+      });
+  }
+
+  function authenticateSpotify() {
+    // REPLACE CLIENT ID
+    const clientId = "32d73e004d394cde9908ed44bb84ecf0";
+    const clientSecret = "f791c6b679b9427eb385a0ea33e44c92";
+
+    const base64Credentials = btoa(`${clientId}:${clientSecret}`);
+
+    return axios
+      .post(
+        "https://accounts.spotify.com/api/token",
+        "grant_type=client_credentials",
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${base64Credentials}`,
+          },
+        }
+      )
+      .then((response) => response.data.access_token)
+      .catch((error) => {
+        console.error("Error authenticating with Spotify:", error);
+        throw error;
+      });
+  }
+
+  function renderMoodPlaylists(playlists) {
+    moodPlaylistsContainer.innerHTML = ""; // Clear previous content
+
+    playlists.forEach((playlist) => {
+      const playlistDiv = document.createElement("div");
+      playlistDiv.style.borderRadius = "1rem";
+      playlistDiv.style.boxShadow = "4px 1px 30px rgba(219, 52, 235)";
+
+      const playlistImage = document.createElement("img");
+      playlistImage.src = playlist.images[0].url;
+      playlistDiv.appendChild(playlistImage);
+
+      const playlistTitle = document.createElement("h3");
+      playlistTitle.textContent = playlist.name;
+      playlistDiv.appendChild(playlistTitle);
+
+      const playlistTracks = document.createElement("p");
+      playlistTracks.textContent = `${playlist.tracks.total} TRACKS`;
+      playlistDiv.appendChild(playlistTracks);
+
+      const loadButton = document.createElement("button");
+      loadButton.textContent = "Load Mood To Playlist";
+      loadButton.addEventListener("click", () => {
+        getPlaylistID(playlist.uri);
+      });
+      playlistDiv.appendChild(loadButton);
+
+      moodPlaylistsContainer.appendChild(playlistDiv);
+    });
+  }
+
+  function getPlaylistID(uri) {
+    selectedPlaylistUri = uri;
+    console.log("Selected Playlist URI:", selectedPlaylistUri);
+
+    // Optionally, you can proceed with loading the selected mood to the playlist
+    loadMoodToPlaylist();
+  }
+
+  function loadMoodToPlaylist() {
+    // Implement load mood to playlist logic here
+  }
+
+  const searchButton = document.getElementById("searchButton");
+  searchButton.addEventListener("click", getSearchInput);
+
+  // ... (Rest of your code)
+});
